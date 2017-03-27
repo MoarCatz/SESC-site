87c65612e05291badde3750e124f78d26f17eb24
Grigory <grigor.smirnow7@yandex.ru>
2017-03-27 21:28:38 +0500

    Adds ScrollSpy for 'Official' section

---
+++ assets/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	//ScrollSpy
+	$('.anchor').on('scrollSpy:enter', function() {
+		$('.nav .nav-item').removeClass('active');
+		$('a[href="#' + $(this).attr('id') + '"]').parent().addClass('active');
+	});
+
+	$('.anchor').on('scrollSpy:exit', function() {
+		//$('a[href="#' + $(this).attr('id') + '"]').parent().removeClass('active');
+	});
+
+	$('.anchor').scrollSpy();
+});
