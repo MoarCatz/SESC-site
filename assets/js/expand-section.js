26dcdfbd714fe1a012fd9d737077b8c171b00d86
Leva7 <leva181777@gmail.com>
2017-03-31 14:47:50 +0500

    :bug: Fixes issues with indentation and responsiveness, also corrects JS dropdowns closes #7 and closes #8

--- assets/js/expand-section.js
+++ assets/js/expand-section.js
@@ -1,8 +1,8 @@
 function expand() {
-  $('#plan + article li + div').hide();
+  $('#plan + article .expandable').hide();
   $('#plan + article button').on('click', function() {
     $(this).next().slideToggle(400);
-    $(this).children('.icon').toggleClass('icon-arrow_drop_up').toggleClass('icon-arrow_drop_down');
+    $(this).children('.icon').toggleClass('icon-keyboard_arrow_right').toggleClass('icon-keyboard_arrow_down');
   });
 }
 
